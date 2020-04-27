ff675c05d57e21956a7dbff88411bcc8e9fe349c
Maxxie <max.umberto97@gmail.com>
2020-04-26 21:56:05 -0300

    Rota para direcionar automaticamente para /users

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function (){
+    return redirect('/users');
+});
 Route::get('/users/search', 'UserController@search');
 Route::resource('/users', 'UserController');
